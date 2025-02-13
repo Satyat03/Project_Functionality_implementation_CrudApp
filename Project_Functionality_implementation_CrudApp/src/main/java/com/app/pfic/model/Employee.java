2a62b4f660d032bde2b976e758f39d2a82425a1a
Satyat03 <101273443+Satyat03@users.noreply.github.com>
2025-02-13 15:24:37 +0530

    Added database by satyat

--- Project_Functionality_implementation_CrudApp/src/main/java/com/app/pfic/model/Employee.java
+++ Project_Functionality_implementation_CrudApp/src/main/java/com/app/pfic/model/Employee.java
@@ -1,11 +1,24 @@
 package com.app.pfic.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
+
+@Entity
 @Data
 public class Employee {
 
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
+	private String username;
+	private String password;
+	private long mobno;
+	private String address;
+	private String adharcard;
+	private String pancard;
 }
--- Project_Functionality_implementation_CrudApp/src/main/java/com/app/pfic/repo/EmployeeRepo.java
+++ Project_Functionality_implementation_CrudApp/src/main/java/com/app/pfic/repo/EmployeeRepo.java
@@ -1,8 +1,11 @@
 package com.app.pfic.repo;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.app.pfic.model.Employee;
+
 @Repository
-public interface EmployeeRepo {
+public interface EmployeeRepo extends JpaRepository<Employee, Integer>{
 
 }
